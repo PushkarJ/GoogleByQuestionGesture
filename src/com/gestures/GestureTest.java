053b36da9d56e574ca9e92a53f270ad9ecfdb3aa
Pushkar D. Joglekar <pushkar.joglekar1789@gmail.com>
2014-04-09 00:06:51 -0400

    Updated the code to add search intent on search image button to perform google search when the button is clicked

--- src/com/gestures/GestureTest.java
+++ src/com/gestures/GestureTest.java
@@ -11,7 +11,7 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-*/
+ */
 
 package com.gestures;
 
@@ -39,6 +39,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,132 +49,155 @@ import com.gestures.utils.Constants;
 
 public class GestureTest extends Activity implements OnGesturePerformedListener {
 
-  private GestureLibrary gestureLib;
-  TextView textView;
-  String selectedText;
-  HashMap<String, Double> results;
-  String searchMethod;
-  int iteration = 0;
-  final int[] targetStart = {146, 560, 613, 90, 604, 542, 217, 359, 236, 195};
-  final int[] targetEnd = {149, 568, 619, 97, 610, 544, 222, 368, 241, 204};
-  private Date endTaskTime;
-  private Date startTaskTime;
-/** Called when the activity is first created. */
+	private GestureLibrary gestureLib;
+	TextView textView;
+	String selectedText;
+	HashMap<String, Double> results;
+	String searchMethod;
+	int iteration = 0;
+	final int[] targetStart = { 146, 560, 613, 90, 604, 542, 217, 359, 236, 195 };
+	final int[] targetEnd = { 149, 568, 619, 97, 610, 544, 222, 368, 241, 204 };
+	private Date endTaskTime;
+	private Date startTaskTime;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-	  
-    super.onCreate(savedInstanceState);
-    Intent intent = getIntent();
-    searchMethod= intent.getExtras().getString(Constants.SEARCH_METHOD);
-    iteration = intent.getExtras().getInt("iteration");
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		Intent intent = getIntent();
+		searchMethod = intent.getExtras().getString(Constants.SEARCH_METHOD);
+		iteration = intent.getExtras().getInt("iteration");
 		results = intent.getExtras().get(Constants.RESULTS) == null ? new HashMap<String, Double>(
 				11) : (HashMap<String, Double>) intent.getExtras().get(
 				Constants.RESULTS);
 
-    GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
-    View inflate = getLayoutInflater().inflate(R.layout.main, null);
-    gestureOverlayView.addView(inflate);
-    gestureOverlayView.setGestureColor(Color.TRANSPARENT);
-    gestureOverlayView.addOnGesturePerformedListener(this);
-    if(searchMethod.equalsIgnoreCase(getResources().getString(R.string.question_mark)))
-    {
-    	gestureLib = GestureLibraries.fromRawResource(this, R.raw.question_gestures);
-    }
-	if (searchMethod.equalsIgnoreCase(getResources().getString(
+		GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
+		View inflate = getLayoutInflater().inflate(R.layout.main, null);
+		gestureOverlayView.addView(inflate);
+		gestureOverlayView.setGestureColor(Color.TRANSPARENT);
+		gestureOverlayView.addOnGesturePerformedListener(this);
+		if (searchMethod.equalsIgnoreCase(getResources().getString(
+				R.string.question_mark))) {
+			gestureLib = GestureLibraries.fromRawResource(this,
+					R.raw.question_gestures);
+		}
+		if (searchMethod.equalsIgnoreCase(getResources().getString(
 				R.string.swirl_anticlockwise))
 				|| searchMethod.equalsIgnoreCase(getResources().getString(
-						R.string.swirl_clockwise)))
-	{
-	    gestureLib = GestureLibraries.fromRawResource(this, R.raw.swirl_gestures);			
-    }
-    if (!gestureLib.load()) {
-      finish();
-    }
-    setContentView(gestureOverlayView);
-    textView = (TextView)findViewById(R.id.hellotext);
-    Button advance = (Button)findViewById(R.id.advance);
-    advance.setOnClickListener(new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-		finish();
-		Intent next = getIntent();
-		next.putExtra("iteration", iteration+1);
-		next.putExtra(Constants.RESULTS, results);
-		startActivity(next);	
+						R.string.swirl_clockwise))) {
+			gestureLib = GestureLibraries.fromRawResource(this,
+					R.raw.swirl_gestures);
 		}
-	});
-    /* Send the hashtable to ShowResults Activity */
-    Button showResults=(Button)findViewById(R.id.show_results);
-    showResults.setVisibility(Button.INVISIBLE);
-    showResults.setOnClickListener(new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			Intent intent = new Intent(GestureTest.this, ShowResults.class);
-			intent.putExtra(Constants.SEARCH_METHOD, searchMethod);
-			intent.putExtra(Constants.RESULTS, results);
-			startActivity(intent); 			
+		if (gestureLib!=null && !gestureLib.load()) {
+			finish();
 		}
-	});
-    if (iteration == 9) {
-    	advance.setEnabled(false);
-    	showResults.setVisibility(Button.VISIBLE);
-    }
-    /* Bold the target text */
-    final SpannableStringBuilder sb = new SpannableStringBuilder(textView.getText().toString());
-    final StyleSpan bdit = new StyleSpan(android.graphics.Typeface.BOLD_ITALIC);
-    sb.setSpan(bdit, targetStart[iteration], targetEnd[iteration], Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-    textView.setText(sb);
-    
-    textView.setOnTouchListener(new OnTouchListener()
-	{		
-		@Override
-		public boolean onTouch(View v, MotionEvent event)
-		{
-			//TODO: rename date variable
-			startTaskTime= new Date();
-		    Log.i("customgestures","Start Timer:"+startTaskTime.toString());			
-		    Log.i("customgestures", "Start:"+textView.getSelectionStart()+ " End: "+textView.getSelectionEnd());
-		    selectedText = textView.getText().subSequence(textView.getSelectionStart(), textView.getSelectionEnd()).toString();
+		setContentView(gestureOverlayView);
+		textView = (TextView) findViewById(R.id.hellotext);
+		Button advance = (Button) findViewById(R.id.advance);
+		advance.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+				Intent next = getIntent();
+				next.putExtra("iteration", iteration + 1);
+				next.putExtra(Constants.RESULTS, results);
+				startActivity(next);
+			}
+		});
+		/* Send the hashtable to ShowResults Activity */
+		Button showResults = (Button) findViewById(R.id.show_results);
+		showResults.setVisibility(Button.INVISIBLE);
+		showResults.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(GestureTest.this, ShowResults.class);
+				intent.putExtra(Constants.SEARCH_METHOD, searchMethod);
+				intent.putExtra(Constants.RESULTS, results);
+				startActivity(intent);
+			}
+		});
+		if (iteration == 9) {
+			advance.setEnabled(false);
+			showResults.setVisibility(Button.VISIBLE);
+		}
+		/* Bold the target text */
+		final SpannableStringBuilder sb = new SpannableStringBuilder(textView
+				.getText().toString());
+		final StyleSpan bdit = new StyleSpan(
+				android.graphics.Typeface.BOLD_ITALIC);
+		sb.setSpan(bdit, targetStart[iteration], targetEnd[iteration],
+				Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+		textView.setText(sb);
+
+		textView.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO: rename date variable
+				startTaskTime = new Date();
+				Log.i("customgestures",
+						"Start Timer:" + startTaskTime.toString());
+				Log.i("customgestures", "Start:" + textView.getSelectionStart()
+						+ " End: " + textView.getSelectionEnd());
+				selectedText = textView
+						.getText()
+						.subSequence(textView.getSelectionStart(),
+								textView.getSelectionEnd()).toString();
 				return false;
 
-		}
-	});
-    
-  }
+			}
+		});
+		ImageButton button = (ImageButton) findViewById(R.id.search_simulate);
+		button.setOnClickListener(new OnClickListener() {
 
-  @Override
-  public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
-    ArrayList<Prediction> predictions = gestureLib.recognize(gesture);
-    int index=0;
-    Prediction prediction;
+			@Override
+			public void onClick(View v) {
+				EditText searchTerm=(EditText)findViewById(R.id.editText1);				
+				Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+				String keyword = searchTerm.getText().toString();
+				intent.putExtra(SearchManager.QUERY, keyword);
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+		ArrayList<Prediction> predictions = gestureLib.recognize(gesture);
+		int index = 0;
+		Prediction prediction;
 		// Go through all the gestures in the library till you find the one
 		// which has a higher prediction score
 		do {
 			prediction = predictions.get(index++);
 		} while (prediction.score < 1.0 && index < predictions.size());
 
-    	if(index < 4 )
-    	{
-    	  //Log.i("customgestures","Stop Timer:"+new Date().toString());
-    	Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-        String keyword=selectedText;
-        intent.putExtra(SearchManager.QUERY, keyword);
-        startActivity(intent);
-        //TODO:rename date variable
-        endTaskTime= new Date();
-        double timetoCompleteTask = ((double)(endTaskTime.getTime() - startTaskTime.getTime()));
-        Log.i("customgestures", "Time to complete the task: "+String.valueOf(timetoCompleteTask));
-        results.put(String.valueOf(iteration), Double.valueOf((timetoCompleteTask/1000)));
-        //Toast.makeText(this, prediction.name, Toast.LENGTH_SHORT)
-          //  .show();
-    	}
-    	else
-    	{
-    		Toast.makeText(this, "Unrecognized gesture detected. Current search method is: "+searchMethod, Toast.LENGTH_LONG).show();
-    	}
-   }
- } 
+		if (index < 4) {
+			// Log.i("customgestures","Stop Timer:"+new Date().toString());
+			Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+			String keyword = selectedText;
+			intent.putExtra(SearchManager.QUERY, keyword);
+			startActivity(intent);
+			// TODO:rename date variable
+			endTaskTime = new Date();
+			double timetoCompleteTask = ((double) (endTaskTime.getTime() - startTaskTime
+					.getTime()));
+			Log.i("customgestures",
+					"Time to complete the task: "
+							+ String.valueOf(timetoCompleteTask));
+			results.put(String.valueOf(iteration),
+					Double.valueOf((timetoCompleteTask / 1000)));
+			// Toast.makeText(this, prediction.name, Toast.LENGTH_SHORT)
+			// .show();
+		} else {
+			Toast.makeText(
+					this,
+					"Unrecognized gesture detected. Current search method is: "
+							+ searchMethod, Toast.LENGTH_LONG).show();
+		}
+	}
+}
